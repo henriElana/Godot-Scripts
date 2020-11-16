1f45980fee2527f810d29b1d5a453366031a14f3
henriElana <70268156+henriElana@users.noreply.github.com>
2020-11-16 09:32:53 +0100

    Add files via upload
    Aaprism utility script can create non colliding prisms (collisions enabled by default).

--- 3D Game/scripts_library/aa_prism.gd
+++ 3D Game/scripts_library/aa_prism.gd
@@ -15,15 +15,16 @@ func _ready():
 
 # Create prismatic StaticBody with MeshInstance and CollisionShape. Local coordinates !
 func build_centered(center_ = Vector3.ZERO, size_ = Vector3.ONE,
-			 parent_ = self, mat_ = m_gray_v90):
+			 parent_ = self, mat_ = m_gray_v90, has_collision_shape = true):
 	var cube_: StaticBody = StaticBody.new()
 	parent_.add_child(cube_)
 	cube_.set_translation(center_) # Local coordinates !
 	
-	var collision_shape_: CollisionShape = CollisionShape.new()
-	cube_.add_child(collision_shape_)
-	collision_shape_.shape = BoxShape.new()
-	collision_shape_.shape.set_extents(Vector3(0.5*size_.x, 0.5*size_.y, 0.5*size_.z))
+	if has_collision_shape:
+		var collision_shape_: CollisionShape = CollisionShape.new()
+		cube_.add_child(collision_shape_)
+		collision_shape_.shape = BoxShape.new()
+		collision_shape_.shape.set_extents(Vector3(0.5*size_.x, 0.5*size_.y, 0.5*size_.z))
 	
 	var mesh_instance_: MeshInstance = MeshInstance.new()
 	cube_.add_child(mesh_instance_)
@@ -33,32 +34,33 @@ func build_centered(center_ = Vector3.ZERO, size_ = Vector3.ONE,
 
 
 # Create axis aligned prism within extents with given material. Local coordinates !
-func build_extents(start_ = Vector3.ZERO, end_ = Vector3.ONE, parent_ = self, mat_ = m_gray_v90):
+func build_extents(start_ = Vector3.ZERO, end_ = Vector3.ONE,
+			 parent_ = self, mat_ = m_gray_v90, has_collision_shape = true):
 	var center_ = 0.5*(start_ + end_)
 	var size_ = Vector3(abs(end_.x-start_.x), abs(end_.y-start_.y), abs(end_.z-start_.z))
 	
-	build_centered(center_, size_, parent_, mat_ )
+	build_centered(center_, size_, parent_, mat_, has_collision_shape)
 
 
 # Create axis aligned prism above center with given material. Local coordinates !
 func build_above(base_center_ = Vector3.ZERO, size_ = Vector3.ONE,
-			 parent_ = self, mat_ = m_gray_v90):
+			 parent_ = self, mat_ = m_gray_v90, has_collision_shape = true):
 	var center_ = base_center_ + Vector3(0, 0.5*abs(size_.y), 0)
 	
-	build_centered(center_, size_, parent_, mat_ )
+	build_centered(center_, size_, parent_, mat_, has_collision_shape)
 	
 
 # Create axis aligned prism below center with given material. Local coordinates !
 func build_below(top_center_ = Vector3.ZERO, size_ = Vector3.ONE,
-			 parent_ = self, mat_ = m_gray_v90):
+			 parent_ = self, mat_ = m_gray_v90, has_collision_shape = true):
 	var center_ = top_center_ + Vector3(0, -0.5*abs(size_.y), 0)
 	
-	build_centered(center_, size_, parent_, mat_ )
+	build_centered(center_, size_, parent_, mat_, has_collision_shape)
 
 
 
 func random_grounded(cuts_ = Vector3.ONE,center_ = Vector3.ZERO, size_ = Vector3.ONE,
-			 parent_ = self, mat_ = m_gray_v90):
+			 parent_ = self, mat_ = m_gray_v90, has_collision_shape = true):
 	# Unit cell size
 	var cellsize_ = Vector3(size_.x/(cuts_.x+1), size_.y/(cuts_.y+1), size_.z/(cuts_.z+1))
 	# Random multiplicators
@@ -79,12 +81,12 @@ func random_grounded(cuts_ = Vector3.ONE,center_ = Vector3.ZERO, size_ = Vector3
 	center_.z += rng.randi_range(0,int(cuts_.z+1)-z_cells_)*cellsize_.z
 	
 	# Build !
-	build_centered(center_, final_cellsize_, parent_, mat_ )
+	build_centered(center_, final_cellsize_, parent_, mat_, has_collision_shape)
 
 
 
 func random_grounded_small(cuts_ = Vector3.ONE,center_ = Vector3.ZERO, size_ = Vector3.ONE,
-			 parent_ = self, mat_ = m_gray_v90):
+			 parent_ = self, mat_ = m_gray_v90, has_collision_shape = true):
 	# Unit cell size
 	var cellsize_ = Vector3(size_.x/(cuts_.x+1), size_.y/(cuts_.y+1), size_.z/(cuts_.z+1))
 	# Random multiplicators
@@ -105,11 +107,11 @@ func random_grounded_small(cuts_ = Vector3.ONE,center_ = Vector3.ZERO, size_ = V
 	center_.z += rng.randi_range(0,int(cuts_.z+1)-z_cells_)*cellsize_.z
 	
 	# Build !
-	build_centered(center_, final_cellsize_, parent_, mat_ )
+	build_centered(center_, final_cellsize_, parent_, mat_, has_collision_shape)
 
 
 func random_free(cuts_ = Vector3.ONE,center_ = Vector3.ZERO, size_ = Vector3.ONE,
-			 parent_ = self, mat_ = m_gray_v90):
+			 parent_ = self, mat_ = m_gray_v90, has_collision_shape = true):
 	# Unit cell size
 	var cellsize_ = Vector3(size_.x/(cuts_.x+1), size_.y/(cuts_.y+1), size_.z/(cuts_.z+1))
 	# Random multiplicators
@@ -131,12 +133,12 @@ func random_free(cuts_ = Vector3.ONE,center_ = Vector3.ZERO, size_ = Vector3.ONE
 	center_.z += rng.randi_range(0,int(cuts_.z+1)-z_cells_)*cellsize_.z
 	
 	# Build !
-	build_centered(center_, final_cellsize_, parent_, mat_ )
+	build_centered(center_, final_cellsize_, parent_, mat_, has_collision_shape)
 
 
 
 func random_free_small(cuts_ = Vector3.ONE, center_ = Vector3.ZERO, size_ = Vector3.ONE,
-			 parent_ = self, mat_ = m_gray_v90):
+			 parent_ = self, mat_ = m_gray_v90, has_collision_shape = true):
 	# Unit cell size
 	var cellsize_ = Vector3(size_.x/(cuts_.x+1), size_.y/(cuts_.y+1), size_.z/(cuts_.z+1))
 	# Random multiplicators
@@ -158,4 +160,4 @@ func random_free_small(cuts_ = Vector3.ONE, center_ = Vector3.ZERO, size_ = Vect
 	center_.z += rng.randi_range(0,int(cuts_.z+1)-z_cells_)*cellsize_.z
 	
 	# Build !
-	build_centered(center_, final_cellsize_, parent_, mat_ )
+	build_centered(center_, final_cellsize_, parent_, mat_, has_collision_shape)
