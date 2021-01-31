a9a418257d05736b8939ac200794468e256520b4
henriElana <70268156+henriElana@users.noreply.github.com>
2021-01-31 18:18:46 +0100

    Add files via upload

---
+++ 3D Game/scripts_library/collision_layer_mask_manager.gd
@@ -0,0 +1,33 @@
+extends Area
+
+# Copy and paste functions to relevant scripts. 2D and  3D.
+
+# Player : layer 1 - mask 2, 3, 4, 5
+# Terrain : layer 2 - mask 1, 3, 4
+# Mobs : layer 3 - mask 1, 2, 3, 4
+# Weapons : layer 4 - mask 1, 2, 3
+# Pickups / traps : layer 5 - mask 1
+
+func setup_player_layer_mask():
+	set_collision_layer(1)
+	set_collision_mask(30)
+	
+	
+func setup_terrain_layer_mask():
+	set_collision_layer(2)
+	set_collision_mask(13)
+	
+	
+func setup_mob_layer_mask():
+	set_collision_layer(4)
+	set_collision_mask(15)
+	
+	
+func setup_weapon_layer_mask():
+	set_collision_layer(8)
+	set_collision_mask(7)
+	
+	
+func setup_pickuptrap_layer_mask():
+	set_collision_layer(16)
+	set_collision_mask(1)
