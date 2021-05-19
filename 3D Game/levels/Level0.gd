c57ae6e4e2d076d852e6f020cef15b36a4b03ea8
henriElana <70268156+henriElana@users.noreply.github.com>
2021-05-19 15:44:02 +0200

    Add files via upload

--- 3D Game/levels/Level0.gd
+++ 3D Game/levels/Level0.gd
@@ -2,15 +2,22 @@ extends Spatial
 
 
 # Declare member variables here.
-var player = preload("res://player/Player_1.tscn")
-var terrain_manager_script = preload("res://levels/plate_juggler.gd")
-var light = preload("res://lights/Fog.tscn")
+onready var player = preload("res://player/Player_1.tscn")
+onready var terrain_manager_script = preload("res://levels/plate_juggler.gd")
+onready var light = preload("res://lights/Fog.tscn")
 var terrain_manager
 
 var goal_position = Vector3.UP*100
 var goal_timer: Timer
 
 onready var gui = $GUI
+
+# Effecs management
+onready var muzzle_flash = preload("res://visual_effects/Muzzle Flash.tscn")
+onready var impact = preload("res://visual_effects/Impact.tscn")
+onready var mortar_shell = preload("res://weapons/MortarShell.tscn")
+onready var mortar_blast = preload("res://weapons/MortarBlast.tscn")
+onready var smash_blast = preload("res://weapons/SmashBlast.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,7 +33,7 @@ func _process(delta):
 func setup_player():
 	player = player.instance()
 	add_child(player)
-	player.translate(Vector3.UP*100.0)
+	player.set_translation(Vector3.UP*100.0)
 
 func setup_terrain():
 	terrain_manager = Spatial.new()
@@ -53,3 +60,41 @@ func setup_goal_timer():
 func on_goal_timer_timeout():
 	var _direction = player.get_arrow(goal_position)
 	gui.orient_arrow(_direction)
+
+func player_dead():
+	pass
+
+func update_energy(_value):
+	gui.update_energy(_value)
+
+func update_life(_value):
+	gui.update_life(_value)
+
+func add_muzzle_flash(_position, _direction):
+	var ob_ = muzzle_flash.instance()
+	add_child(ob_)
+	ob_.look_at_from_position(_position, _position + _direction, Vector3.UP)
+	
+
+func add_impact(_position, _direction):
+	var ob_ = impact.instance()
+	add_child(ob_)
+	ob_.look_at_from_position(_position, _position + _direction, Vector3.ONE)
+
+func add_mortatblast(_position):
+	var mb_ = mortar_blast.instance()
+	add_child(mb_)
+	mb_.set_translation(_position)
+
+func add_smashblast(_position):
+	var sb_ = smash_blast.instance()
+	add_child(sb_)
+	sb_.set_translation(_position)
+	
+func add_mortarshell(_position):
+	var ms_ = mortar_shell.instance()
+	add_child(ms_)
+	ms_.set_translation(_position)
+
+func add_bolt(_position, _direction):
+	pass
